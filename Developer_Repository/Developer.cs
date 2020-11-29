67ccd9bc9380af90033619cb10006ebeb7b43c2d
Michael Shepard <mike@rockcastlepartners.com>
2020-11-29 16:12:29 -0500

    new files added

---
+++ Developer_Repository/Developer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Developer_Repository
+{
+    class Class1
+    {
+    }
+}
---
+++ KomodoInsurance/ProgramUI.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KomodoInsurance_Console
+{
+    class Class1
+    {
+    }
+}
